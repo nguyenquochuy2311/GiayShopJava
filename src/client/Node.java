a97481c1ba075b1b0ce64ac86837db8885e20767
Huy Nguyen <52172667+nguyenquochuy2311@users.noreply.github.com>
2021-11-23 16:20:19 +0700

    commit

--- src/client/Node.java
+++ src/client/Node.java
@@ -1,11 +1,11 @@
 package client;
 
 public class Node<T> {
-	 T data;
-	 Node<T> left;
-	 Node<T> right;
+	  T data;
+	  Node<T> left;
+	  Node<T> right;
 
-	 public Node(T data) {
-		 this.data = data;
-	 }
+	  public Node(T data) {
+	    this.data = data;
+	  }
 }
